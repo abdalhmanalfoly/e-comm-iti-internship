54405cc460a9087a81eac80a0573e27d8d752eec
abdalrhman <radea4082@gmail.com>
2026-05-01 17:34:53 +0300

    first commit

---
+++ js/checkout.js
@@ -0,0 +1,55 @@
+
+import { getData, setData } from './storage.js';
+import { clearCart, getCartItems, getCartSubtotal } from './cart.js';
+import { getCurrentUser } from './auth.js';
+
+export function placeOrder(shippingInfo) {
+  if(!getCurrentUser){
+    return 0 // redirect to login page 
+  }else{
+
+    const cartItems = getCartItems();
+  }
+  if (cartItems.length === 0) return { success: false, message: "Cart is empty." };
+
+  const subtotal = getCartSubtotal();
+  const tax = subtotal * 0.1; // 10% tax
+  const shipping = 5.00;
+  const total = subtotal + tax + shipping;
+
+  const user = getCurrentUser();
+  const orders = getData("orders");
+
+  const newOrder = {
+    id: `ORD-${Date.now()}`,
+    userId: user ? user.id : 'gust',
+    items: cartItems,
+    shippingInfo,
+    summary: {
+      subtotal,
+      tax,
+      shipping,
+      total
+    },
+    status: 'Pending',
+    createdAt: new Date().toISOString()
+  };
+
+  orders.push(newOrder);
+  setData("orders", orders);
+  clearCart();
+
+  return { success: true, orderId: newOrder.id };
+}
+
+export function getUserOrders(userId) {
+  const orders = getData("orders");
+  return orders.filter(order => order.userId === userId);
+}
+
+export function deleteOrder(orderId) {
+  let orders = getData("orders");
+  orders = orders.filter(order => order.id !== orderId);
+  setData("orders", orders);
+  return true;
+}
